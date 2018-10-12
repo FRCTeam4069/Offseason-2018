7d0ee1dfb22b2ce90524a81495fc7cd017696b86
Redrield <redrield@gmail.com>
2018-10-12 11:30:04 -0400

    no more breaking stuff thanks

--- src/main/kotlin/frc/team4069/robot/commands/elevator/OperatorControlElevatorCommand.kt
+++ src/main/kotlin/frc/team4069/robot/commands/elevator/OperatorControlElevatorCommand.kt
@@ -2,29 +2,36 @@ package frc.team4069.robot.commands.elevator
 
 import com.ctre.phoenix.motorcontrol.ControlMode
 import edu.wpi.first.wpilibj.command.Command
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import frc.team4069.robot.OI
 import frc.team4069.robot.subsystems.ElevatorSubsystem
 
 class OperatorControlElevatorCommand : Command() {
 
     private var set = true
+    var isDemo = false
 
     init {
         requires(ElevatorSubsystem)
     }
 
     override fun initialize() {
+        isDemo = SmartDashboard.getBoolean("Demo Mode", false)
         println("Operator command init")
     }
 
     override fun execute() {
         val elevatorAxis = OI.elevatorAxis
 
-        if(elevatorAxis != 0.0) {
-            ElevatorSubsystem.set(ControlMode.PercentOutput, elevatorAxis)
+        if (elevatorAxis != 0.0) {
+            if (isDemo) {
+                ElevatorSubsystem.set(ControlMode.PercentOutput, elevatorAxis * 0.3)
+            } else {
+                ElevatorSubsystem.set(ControlMode.PercentOutput, elevatorAxis)
+            }
             set = false
-        }else {
-            if(!set) {
+        } else {
+            if (!set) {
                 ElevatorSubsystem.set(ControlMode.MotionMagic, ElevatorSubsystem.position.toDouble())
                 set = true
             }
