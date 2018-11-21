2fef1604282ca7795012a9584683733ab2977056
brendon-ai <brendon-m@outlook.com>
2018-11-21 12:46:09 -0500

    Nyooming is v nice

--- src/main/kotlin/frc/team4069/robot/commands/drive/FollowTapeCommand.kt
+++ src/main/kotlin/frc/team4069/robot/commands/drive/FollowTapeCommand.kt
@@ -7,7 +7,7 @@ import frc.team4069.saturn.lib.math.uom.velocity.VelocityUnit
 import frc.team4069.saturn.lib.math.uom.velocity.fps
 import frc.team4069.robot.subsystems.DriveBaseSubsystem as driveBase
 
-class FollowTapeCommand(val baseVelocity: VelocityUnit = 0.3.fps, val correctionFactor: Double = 1.0) : Command() {
+class FollowTapeCommand(val baseVelocity: VelocityUnit = 0.3.fps, val tapeP: Double = 0.75, val posP: Double = 2.0) : Command() {
 
     var lTraveledLast: Double = 0.0
     var rTraveledLast: Double = 0.0
@@ -21,26 +21,32 @@ class FollowTapeCommand(val baseVelocity: VelocityUnit = 0.3.fps, val correction
     }
 
     override fun execute() {
-        val speedDelta = (VisionSystem.tapeX.toDouble() * correctionFactor) / VisionSystem.width.toDouble()
-        val lCoef = 1 + speedDelta
-        val rCoef = 1 - speedDelta
-//        println("L:")
-//        println(lCoef)
-//        println("R:")
-//        println(rCoef)
+        val speedDelta = (VisionSystem.tapeX.toDouble() * tapeP) / VisionSystem.width.toDouble()
+        val expectedLCoef = 1 + speedDelta
+        val expectedRCoef = 1 - speedDelta
         val lDelta = driveBase.leftPosition.ft - lTraveledLast
         val rDelta = driveBase.rightPosition.ft - rTraveledLast
-//        println(lDelta)
-//        println(rDelta)
-        println(driveBase.leftPosition.ft)
-        println(driveBase.rightPosition.ft)
         val meanDelta = (lDelta + rDelta) / 2
-        val lCoefActual = lDelta / meanDelta
-        val rCoefActual = rDelta / meanDelta
-        driveBase.set(ControlMode.PercentOutput, baseVelocity.fps * lCoef, baseVelocity.fps * rCoef)
+        val outputLCoef: Double
+        val outputRCoef: Double
+        if (meanDelta == 0.0) {
+            outputLCoef = expectedLCoef
+            outputRCoef = expectedRCoef
+        } else {
+            val measuredLCoef = lDelta / meanDelta
+            val measuredRCoef = rDelta / meanDelta
+            val lCoefError = measuredLCoef - expectedLCoef
+            val rCoefError = measuredRCoef - expectedRCoef
+            outputLCoef = expectedLCoef - (lCoefError * posP)
+            outputRCoef = expectedRCoef - (rCoefError * posP)
+        }
+        println(outputLCoef)
+        println(outputRCoef)
+        println("separator")
+        driveBase.set(ControlMode.PercentOutput, baseVelocity.fps * outputLCoef, baseVelocity.fps * outputRCoef)
 
         lTraveledLast = driveBase.leftPosition.ft
-        lTraveledLast = driveBase.leftPosition.ft
+        rTraveledLast = driveBase.rightPosition.ft
     }
 
     override fun isFinished(): Boolean {
